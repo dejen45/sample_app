3ac1ae40883f7152636a26430212a5cb127f6c09
dejen45 <dejen45@yahoo.com>
2012-06-11 01:37:49 -0500

    Added Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,17 @@ describe "About page" do
 	end
 	end
 
+describe "Contact Page" do
+	it "should have the h1 'Contact'" do
+		visit '/static_pages/contact'
+		page.should have_selector('h1', :text =>'Contact')
+	end
+	
+	it "should have the title 'Contact'" do
+		visit '/static_pages/contact'
+		page.should have_selector('title',
+						:text => "Ruby on Rails Tutorial Sample App | Contact")
+	end
+	end
+
 end
